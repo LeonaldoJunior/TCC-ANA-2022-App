ab17cecf9b482ee1e79fc77765aba8afd677a8a4
Leonaldo B Junior <leonaldo.barbosa@gmail.com>
2021-09-04 11:54:07 -0300

    add MyLevelPage

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 
 import React from 'react';
+
 import { WelcomePage } from './pages/WelcomePage';
+import { MyLevelPage } from './pages/MyLevelPage';
 
 export default function App() {
   return (
-    <WelcomePage></WelcomePage>
+    // <WelcomePage></WelcomePage>
+    <MyLevelPage></MyLevelPage>
   );
 }
 
